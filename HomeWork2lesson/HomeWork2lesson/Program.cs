e491278e22947019475c5d78bb152cfdab694b9b
Oleg <traicer95@gmail.com>
2021-12-24 12:07:42 +0700

    Finished

---
+++ HomeWork2lesson/HomeWork2lesson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace HomeWork2lesson
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Введите минимальную температуру за сутки:");
+            double tMin = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите максимальную температуру за сутки:");
+            double tMax = Convert.ToDouble(Console.ReadLine());
+            double tAverage = (tMax + tMin) / 2;
+            Console.WriteLine($"Среднесуточная температура составляет {tAverage} градусов");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
