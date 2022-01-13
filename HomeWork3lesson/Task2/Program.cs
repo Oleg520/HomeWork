d66d6c902d5623c5cdb82dc72be69f6fd0e5df8a
Oleg <traicer95@gmail.com>
2022-01-13 13:52:51 +0700

    Finished

---
+++ HomeWork3lesson/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] phonebook = new string[,]
+            {
+                { "Vasya" , "vasilek@mail.ru"},
+                { "Kostya", "konstantine@icloud.com"},
+                { "Vladimir", "vovkamyaso@gmail.com"},
+                { "Katya" , "katakomba@mail.ru"},
+                { "Natalya", "krasotka228@bk.ru"}
+            };
+
+            for (int i = 0; i < phonebook.GetLength(0); i++)
+            {
+                for (int j = 0; j < phonebook.GetLength(1); j++)
+                {
+                    Console.Write($"{phonebook[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork3lesson/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string reverse = Console.ReadLine();
+            for (int i = reverse.Length - 1; i >= 0; i--)
+                Console.Write(reverse[i]);
+            Console.ReadKey();
+        }
+    }
+}
